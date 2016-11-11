265b73ca42b5ec2976acbcbc0d10aaebe0fd17b0
zachnurc <zach.nurcombe@gmail.com>
2016-11-11 14:54:07 +0000

    changed position of language selector

--- partials/_header.php
+++ partials/_header.php
@@ -7,13 +7,4 @@
     Signed in as Michael Boss -
     <a href="session/logout.php">Log out</a>
   </div>
-  <div id="language_selector" class="dropdown_container">
-    <label for="language_dropdown_controller" class="dropdown_label">English</label>
-    <input type="checkbox" id="language_dropdown_controller" class="dropdown_controller" />
-    <div class="dropdown_contents">
-      <a href="https://make-it-all-demo.herokuapp.com">English</a>
-      <a href="https://make-it-all-demo-arabic.herokuapp.com">Arabic</a>
-      <a href="https://make-it-all-demo-chinese.herokuapp.com">Chinese</a>
-    </div>
-  </div>
 </div>
--- partials/_sidebar.php
+++ partials/_sidebar.php
@@ -19,4 +19,10 @@
     <a href="#">Look at me</a>
     <a href="#">Nothing here</a>
   </section>
+  <div id="language_selector">
+    <a href="https://make-it-all-demo.herokuapp.com"><img src="assets/images/eng-flag.png"/></a>
+    <a href="https://make-it-all-demo-arabic.herokuapp.com"><img src="assets/images/arabic-flag.png"/></a>
+    <a href="https://make-it-all-demo-german.herokuapp.com"><img src="assets/images/german-flag.png"/></a>
+    <a href="https://make-it-all-demo-chinese.herokuapp.com"><img src="assets/images/chinese-flag.png"/></a>
+  </div>
 </div>
